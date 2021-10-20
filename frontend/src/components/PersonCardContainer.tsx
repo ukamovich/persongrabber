0f520846657e830f09b186105839e16d0fbf4659
Håkon Hegre <haakoheg@stud.ntnu.no>
2021-10-20 15:47:36 +0200

    implemented filtering tools #17

--- frontend/src/components/PersonCardContainer.tsx
+++ frontend/src/components/PersonCardContainer.tsx
@@ -4,34 +4,65 @@ import React, { useState, useEffect } from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import fetchGrabber from "../_helpers/fetchGrabber";
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
 
-// denne skal represemtere data basert på query.
 let port = 3001 || process.env.REACT_APP_BACKEND_PORT
 let backendURL = `http://localhost:${port}/graphql`
+
 function PersonCardContainer() {
 
-
+    const [count, setCount] = React.useState(1);
+    const [checked, setChecked] = React.useState(false);
+    const [sort, setSort] = React.useState('undefined');
+    const [optionValue, setOptionValue] = React.useState(""); // sender dette til field i query
     const [search, setSearch] = React.useState(''); 
-    const [exe, setExe] = React.useState('');
+    const [exe, setExe] = React.useState('un2ds3rf22223dssdsdds23fds223ew23233w2w23w343rgn11'); // sikrer at ikke alle persons blir loadet med en gang.
     const [currentPage, setCurrentPage] = React.useState(1);
     const [data, setData] = React.useState([{
-        "_id": "dummy",
-        "first_name": "dummy",
-        "last_name": "dummy",
-        "gender": "dummy",
-        "birthdate": "dummy"
+        "_id": "",
+        "first_name": "",
+        "last_name": "",
+        "gender": "",
+        "birthdate": ""
       }])
+
     const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
         setCurrentPage(value);   
-        // må nå si ifra til backend om den rangen vi vil ha data fra 
-        // for enkelthetens skyld så sier vi kun 1 card per page for testing.
     };
+
+    const handleChangeEvent = (event: SelectChangeEvent) => {
+        setOptionValue(event.target.value);
+        console.log(event.target.value);
+
+      };
+
+      const handleChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+        if (checked === true) {
+            setSort('asc');
+        }
+
+        else {
+            setSort('desc');
+        }
+
+        console.log("Tilstand på sortering: " + sort);
+      };
+
+
 
     useEffect(() => { //"asc", "desc", e.g. people(sort: {value: "asc", field: "first_name"})
         let queryBody = {
             query: `
                 query {
-                    people(page:${currentPage}, search: {value: "${exe}", field:"first_name"}) {
+                    people(page:${currentPage}, search: {value: "${exe}", field:"${optionValue}"}) {
                         _id
                         first_name
                         last_name
@@ -42,23 +73,66 @@ function PersonCardContainer() {
             `
         }
         fetchGrabber(queryBody, backendURL).then(res => {
-            setData(res.data.people)
+            setData(res.data.people);
+            
         })
     }) 
 
     return(
 
+
         <div className="container">
 
-            <div className="btn-group" role="group" aria-label="Basic example">
-                <input type="text" className="form-control mt-3" required placeholder="Name" name="name" onChange={event => setSearch(event.target.value)}/>
-                <button className="primary-btn" onClick = {event => setExe(search)} >Search </button>
+            <div style={{ width: '100%' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', p: 2, bgcolor: 'red.300', borderColor: "white"}}>
 
-            </div>
+                 <Box sx={{ p: 1, order: 2, bgcolor: 'white.300' }}>
+                 <TextField id="outlined-basic" sx={{color: 'success.main'}} label="Search" variant="outlined" onChange={event => setSearch(event.target.value)} />
+                </Box>
+
+
+                <Box sx={{ p: 1, order: 2, bgcolor: 'white .300' }}>
+
+
+                <FormControl sx={{minWidth: 120, color: 'third' }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Filter</InputLabel>
+                <Select
+                labelId="demo-simple-select-autowidth-label"
+                id="demo-simple-select-autowidth"
+                value={optionValue}
+                onChange={handleChangeEvent}
+                autoWidth = {true}
+                label="Age"
+                
+        >
+                <MenuItem  value={"first_name"}>Name</MenuItem>
+                 <MenuItem value={"gender"}>Gender</MenuItem>
+                <MenuItem value={"age"}>Age</MenuItem>
+                </Select>
+                </FormControl>
+                </Box>
+
+
+                <Box sx={{ p: 1, order: 2, bgcolor: 'white.300' }}>
+                <Checkbox checked={checked} onChange={handleChecked} inputProps={{ 'aria-label': 'controlled' }}
+    />
+                </Box>
+
+
+                <Box sx={{ p: 1, order: 2, bgcolor: 'white.300' }}>
+                    <Button variant="contained" onClick = {event => setExe(search)}>Search</Button>
+                </Box>
+
+
+
+                        </Box>
+                             </div>
+
+
 
             <div className="row">
 
-                {data && data.map(el => {
+                {data.length > 1 && data.map(el => {
                     return <PersonCard name= {el.first_name + " " + el.last_name} birthdate = {el.birthdate} gender={el.gender} key={el._id}></PersonCard>
                     
                 })}
@@ -69,18 +143,22 @@ function PersonCardContainer() {
                 {/* Also style for better experience */}
 
 
-                <Stack spacing={4}>
-                    <Pagination count={100} page={currentPage} onChange={handleChange} variant="outlined" shape="rounded" size= "large" color="secondary"/>
-                </Stack>
-
-
-
             </div>  
 
 
+
+
+            <Box sx={{ display: 'flex', justifyContent: 'center', p: 2, bgcolor: 'red.300', borderColor: "white"}}>
+                <Box sx={{ p: 5, order: 2, bgcolor: 'white.300' }}>
+                    <Stack spacing={4}>
+                     <Pagination count={count} page={currentPage} onChange={handleChange} variant="outlined" shape="rounded" size= "large" color="secondary"/>
+                         </Stack>
+                    
+                </Box>
+                </Box>
+            
         </div>
 
-       
 
     );
 
