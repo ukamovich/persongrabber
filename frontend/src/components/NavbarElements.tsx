d49070e1e90d50bb427c779c2b8cfad8a82495d1
Håkon Hegre <haakoheg@stud.ntnu.no>
2021-10-22 12:11:06 +0200

    removed hamburgeremny #19

--- frontend/src/components/NavbarElements.tsx
+++ frontend/src/components/NavbarElements.tsx
@@ -3,19 +3,18 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: #63D471;
+background: #5CD85A;
 height: 85px;
 display: flex;
-justify-content: space-between;
+justify-content: start;
 padding: 0.2rem calc((100vw - 1000px) / 2);
 z-index: 12;
-/* Third Nav */
-/* justify-content: flex-start; */
 `;
 
 export const NavLink = styled(Link)`
 color: #808080;
 display: flex;
+style: bold;
 align-items: center;
 text-decoration: none;
 padding: 0 1rem;
@@ -28,16 +27,7 @@ cursor: pointer;
 
 export const Bars = styled(FaBars)`
 display: none;
-color: #808080;
-@media screen and (max-width: 768px) {
-	display: block;
-	position: absolute;
-	top: 0;
-	right: 0;
-	transform: translate(-100%, 75%);
-	font-size: 1.8rem;
-	cursor: pointer;
-}
+color: #;
 `;
 
 export const NavMenu = styled.div`
@@ -49,9 +39,6 @@ margin-right: -24px;
 /* Third Nav */
 /* width: 100vw;
 white-space: nowrap; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
 `;
 
 export const NavBtn = styled.nav`
@@ -61,9 +48,6 @@ margin-right: 24px;
 /* Third Nav */
 /* justify-content: flex-end;
 width: 100vw; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
 `;
 
 export const NavBtnLink = styled(Link)`
