2e575f93aaacd0136f772b96f8cab5c2d7f5e567
Uladzimir Kamovich <uladzimir.kamovich@gmail.com>
2021-10-31 15:00:10 +0100

    Snapshot tests added

--- frontend/src/features/personGrabber/tests/Navbar.test.tsx
+++ frontend/src/features/personGrabber/tests/Navbar.test.tsx
@@ -1,14 +1,29 @@
 import Navbar from "../../Navbar";
 import { shallow } from "enzyme";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import renderer from "react-test-renderer";
 
 describe("Check if navbar renders correctly", () => {
-    test("Check if renders", () => {
-        shallow(<Navbar />)
-    })
+  test("Check if renders", () => {
+    shallow(<Navbar />);
+  });
 
-    test("Check if navbar contains expected elements", () => {
-        const navbar = shallow(<Navbar />)
-        expect(navbar.find(Link)).toHaveLength(3)
-    })
-})
+  test("Check if navbar contains expected elements", () => {
+    const navbar = shallow(<Navbar />);
+    expect(navbar.find(Link)).toHaveLength(3);
+  });
+});
+
+// Snapshot testing with Jest
+describe("Navbar", () => {
+  it("renders properly", () => {
+    const treeHeader = renderer
+      .create(
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      )
+      .toJSON();
+    expect(treeHeader).toMatchSnapshot();
+  });
+});
